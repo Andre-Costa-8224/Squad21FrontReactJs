fb3736e80a2c05109f92735bc58343375ccf22d3
André F S Costa <67339384+Andre-Costa-8224@users.noreply.github.com>
2022-04-02 21:42:16 -0300

    Update rodape.jsx

--- src/Componentes/Rodape/rodape.jsx
+++ src/Componentes/Rodape/rodape.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   var data = new Date()
+  var mes = ["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
   return (
     <footer class="border-top">
       <span>
-        &copy; {data.getDate()+" / "+data.getFullYear()} - Refugiados -{" "}
+        &copy; {data.getDate()+" / "+mes[data.getMonth()]+" / "+data.getFullYear()} - Refugiados -{" "}
         <Link to={"/sobre"}>Sobre</Link>
       </span>
       | <span>Contato: squad21github@gmail.com</span>
